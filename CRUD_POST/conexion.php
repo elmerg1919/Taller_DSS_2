a11b4c8774157059d23eb7a5a4819932f2effa5a
elmerg1919 <61604682+elmerg1919@users.noreply.github.com>
2022-04-03 00:34:33 -0600

    Insertar Datos

---
+++ CRUD_POST/conexion.php
@@ -0,0 +1,23 @@
+<?php 
+function conexion(){
+	$host = "host=localhost";
+	$port = "port=5432";
+	$dbname = "dbname=Alumnos";
+	$user = "user=postgres";
+	$password = "password=12345";
+
+	$db = pg_connect("$host $port $dbname $user $password");
+
+	return $db;
+
+	//PRUEBA DE CONEXION
+	//if ($db) {
+	//	echo "CONEXION EXITOSA";
+	//}
+	//else {
+	//	echo "Error en la conexion: ".pg_last_error();
+	//}
+}
+
+//echo conexion();
+?>
